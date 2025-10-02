a424e448e68ed35a69dad40a3e0ec6223bcedcda
LukeMosley <135234084+LukeMosley@users.noreply.github.com>
2025-10-02 21:09:27 +0930

    Update algal_dashboard.py

--- algal_dashboard.py
+++ algal_dashboard.py
@@ -226,7 +226,7 @@ if not sub_df.empty:
     # ---------------------------
     # Map display (undocked)
     # ---------------------------
-    st_folium(m, width='100%', height=700)
+    st_folium(m, width='100%', height=600)
 
 # ---------------------------
 # Trends Section
