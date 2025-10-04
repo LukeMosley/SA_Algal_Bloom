cccb295a3bfae597d9291052d92f900bfbd1bcfb
LukeMosley <135234084+LukeMosley@users.noreply.github.com>
2025-10-04 18:03:10 +0930

    Update algal_dashboard.py

--- algal_dashboard.py
+++ algal_dashboard.py
@@ -353,7 +353,7 @@ if not df.empty:  # Check full df for options, even if sub_df is filtered
         # Custom formatted title above the chart
         st.markdown(
             "<b>Trends for Selected Species</b> <span style='font-weight: normal;'>(average values will be displayed if 'All Sites' selected in dropdown)</span>", unsafe_allow_html=True
-            )
+        )
         
         st.altair_chart(base, use_container_width=True)
         
